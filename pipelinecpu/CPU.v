c2813a4a4aa669f2e6cbd4a9a50d643ad6a45f45
immortalrover <immortalrover@protonmail.com>
2024-04-12 17:24:37 +0800

    Redo pipelinecpu

--- pipelinecpu/CPU.v
+++ pipelinecpu/CPU.v
@@ -2,10 +2,29 @@
 module CPU (
 	input											clk,
 	input											reset,
-	output	[`AddrWidth-1:0]	pc // AddrWidth = 32
+	output	[`AddrWidth-1:0]	pcReadData // AddrWidth = 32
 );
 
+reg	[`AddrWidth-1:0] PC; // get PC
+initial PC = 0;
+always @(*) if (reset) PC = 0;
+
 wire	[`InstrWidth-1:0]	instr; // InstrWidth = 32
-InstrMem	instrMem(.instrAddr(pc), .instrData(instr));
-Breakdown breakdown(clk, reset, instr, pc);
+InstrMem	instrMem(.instrAddr(pcReadData), .instrData(instr));
+
+wire	[`AddrWidth-1:0]	pcWriteData;
+wire	[2:0]							pcOp;
+Breakdown breakdown(clk, reset, instr, pcReadData, pcWriteData, pcOp);
+
+always @(posedge clk)
+begin
+	case (pcOp)
+		`PCClear:		PC	<=	0;
+		`PCAdd4:		PC	<=	PC + 4;
+		`PCAddImm:	PC	<=	PC + pcWriteData;
+		`PCSetImm:	PC	<=	pcWriteData;
+	endcase
+end
+
+assign pcReadData = PC;
 endmodule

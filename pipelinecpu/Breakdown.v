b82228d0c2c082ff6eeeee09c533d179a4d3ce50
immortalrover <immortalrover@protonmail.com>
2024-04-16 14:48:04 +0800

    Change imm width

--- pipelinecpu/Breakdown.v
+++ pipelinecpu/Breakdown.v
@@ -15,31 +15,42 @@ wire	[4:0]							regWriteNum	= instr[11:7];
 wire	[4:0]							regNum0			= instr[19:15];
 wire	[4:0]							regNum1			= instr[24:20];
 
-reg		[`DataWidth-1:0]	imm;
-initial imm = 0;
-
+reg [`DataWidth-1:0]	immData;
+initial immData = 0;
 always @(*)
 begin
 	case(opcode)
 		7'b0010011, 7'b1100111, 7'b0000011:	// FMT I
 			case(func3)
 				3'b001, 3'b101: 
-					imm = { 20'b0, instr[31:20] };
+					immData = { 20'b0, instr[31:20] };
 				default:
-					imm = { { 20{ instr[31] } }, instr[31:20] };
+					immData = { { 20{ instr[31] } }, instr[31:20] };
 			endcase
 		7'b0100011:	// FMT S
-			imm	= { { 20{ instr[31] } }, instr[31:25], instr[11:7] };
+			immData	= { { 20{ instr[31] } }, instr[31:25], instr[11:7] };
 		7'b1100011: // FMT B
-			imm = { { 20{ instr[31] } }, instr[7], instr[30:25], instr[11:8], 1'b0 };
+			immData = { { 20{ instr[31] } }, instr[7], instr[30:25], instr[11:8], 1'b0 };
 		7'b0010111, 7'b0110111: // FMT U
-			imm = { instr[31:12], 12'b0 };
+			immData = { instr[31:12], 12'b0 };
 		7'b1101111: // FMT J
-			imm = { { 13{ instr[31] } }, instr[19:12], instr[20], instr[30:21], 1'b0 };
+			immData = { { 13{ instr[31] } }, instr[19:12], instr[20], instr[30:21], 1'b0 };
 		default:
-			imm = 32'bz;
+			immData = 32'bz;
 	endcase
 end
 
-Decode ID(clk, reset, opcode, func3, func7, regWriteNum, regNum0, regNum1, imm, pcReadData, pcWriteData, pcOp);
+reg		[2*`DataWidth-1:0]	imm;
+initial imm = 0;
+always @(posedge clk)
+begin
+	imm [2*`DataWidth-1:0]	<= immData;
+	imm [`DataWidth-1:0]		<= imm [2*`DataWidth-1:`DataWidth];
+end
+
+Decode ID(
+	clk, reset, opcode, func3, func7, 
+	regWriteNum, regNum0, regNum1, imm [`DataWidth-1:0], 
+	pcReadData, pcWriteData, pcOp
+);
 endmodule;

3fefd1824d32de12b7743d7eab868b835288d6e9
immortalrover <immortalrover@protonmail.com>
2024-05-11 17:56:41 +0800

    have a try

--- pipelinecpu/CPU.v
+++ pipelinecpu/CPU.v
@@ -4,20 +4,20 @@ module xgriscv_pipeline (
 	output	reg [`AddrWidth-1:0]	PC // AddrWidth = 32
 );
 
-reg		[`AddrWidth-1:0]	pcData[4:0]; // reg for PC
+reg		[`AddrWidth-1:0]	pcData[1:0]; // reg for PC
 reg		[`InstrWidth-1:0]	instrData[1:0]; // reg for instr
 
 wire										hazard, flush, pcWriteEnable;
 wire	[`AddrWidth-1:0]	pcWriteData;
 wire	[`InstrWidth-1:0]	instr; // InstrWidth = 32
 
-initial pcData[3] = -4;
+initial pcData[0] = -4;
 
 always @(*)
 begin
-	PC = pcData[0] - 4;
+	PC = pcData[0] - 16;
 	instrData[1] = instr;
-	pcData[4] = pcData[3] + 4;
+	pcData[1] = pcData[0] + 4;
 end
 
 always @(posedge clk)
@@ -25,15 +25,12 @@ begin
 	if(~hazard)
 	begin
 		instrData[0] <= instrData[1];
-		pcData[3] <= pcWriteEnable ? pcWriteData : pcData[4];
+		pcData[0] <= pcWriteEnable ? pcWriteData : pcData[1];
 	end
-	pcData[0] <= pcData[1];
-	pcData[1] <= pcData[2];
-	pcData[2] <= pcData[3];
 end
-InstrMem U_imem(pcData[3], instr);
+InstrMem U_imem(pcData[0], instr);
 
-Decode ID(clk, reset, flush, pcData[3], instrData[0], hazard, pcWriteEnable, pcWriteData);
+Decode ID(clk, reset, flush, pcData[0], instrData[0], hazard, pcWriteEnable, pcWriteData);
 
 Flush flushing(clk, pcWriteEnable, flush);
 endmodule

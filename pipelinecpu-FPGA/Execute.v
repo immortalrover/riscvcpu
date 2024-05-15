8b7c1bce6b022ac8d0d7c381b1eb341105b75e9d
immortalrover <immortalrover@protonmail.com>
2024-05-15 18:21:29 +0800

    update Execute.v

--- pipelinecpu-FPGA/Execute.v
+++ pipelinecpu-FPGA/Execute.v
@@ -16,45 +16,45 @@ module Execute (
 	output	[`DataWidth-1:0]	regWatchData, aluWatchO, memWatchData
 );
 
-reg												regInEnable[1:0]; // reg for regInEnable
-reg		[`ALUOpWidth-1:0]		aluOp; // ALUOpWidth = 5
-reg		[`AddrWidth-1:0]		pcData[2:0];
-reg		[`DataWidth-1:0]		aluX, aluY,
-													aluOut[1:0], // reg for store alu out data
-													immData[1:0],
-													regInData[1:0], // reg for regWriteData
-													regOutData1[1:0]; // reg for storing rs2 data
-reg		[`Func3Width-1:0]		func3Data[1:0]; // reg for storing func3 data
-reg		[`RegNumWidth-1:0]	regInNum[2:0];
-reg		[`StateWidth-1:0]		state[1:0]; // StateWidth = 4
+reg		[1:0]									regInEnable; // reg for regInEnable
+reg		[`ALUOpWidth-1:0]			aluOp; // ALUOpWidth = 5
+reg		[3*`AddrWidth-1:0]		pcData;
+reg		[`DataWidth-1:0]			aluX, aluY;
+reg		[2*`DataWidth-1:0]		aluOut; // reg for store alu out data
+reg		[2*`DataWidth-1:0]		immData;
+reg		[2*`DataWidth-1:0]		regInData; // reg for regWriteData
+reg		[2*`DataWidth-1:0]		regOutData1; // reg for storing rs2 data
+reg		[2*`Func3Width-1:0]		func3Data; // reg for storing func3 data
+reg		[3*`RegNumWidth-1:0]	regInNum;
+reg		[2*`StateWidth-1:0]		state; // StateWidth = 4
 
-wire											regWriteEnable;
-wire	[1:0]								forwardA, forwardB;
-wire	[`DataWidth-1:0]		aluO, data, regReadData0, regReadData1, regWriteData;
+wire												regWriteEnable;
+wire	[1:0]									forwardA, forwardB;
+wire	[`DataWidth-1:0]			aluO, data, regReadData0, regReadData1, regWriteData;
 
 assign aluWatchO = aluO;
 
 always @(*)
 begin
-	immData[1] = imm;
-	func3Data[1] = func3;
-	aluOut[1] = aluO;
-	pcData[2] = PC;
-	regInData[1] = regWriteData;
-	regInEnable[1] = regWriteEnable;
-	regInNum[2] = regWriteNum;
-	MemReadEnable = state[1] == `MemReadRegWrite;
+	MemReadEnable																		= state[2*`StateWidth-1:`StateWidth] == `MemReadRegWrite;
+	aluOut			[2*`DataWidth-1		:		`DataWidth]		= aluO;
+	func3Data		[2*`Func3Width-1	:		`Func3Width]	= func3;
+	immData			[2*`DataWidth-1		:		`DataWidth]		= imm;
+	pcData			[3*`AddrWidth-1		:	2*`AddrWidth]		= PC;
+	regInData		[2*`DataWidth-1		:		`DataWidth]		= regWriteData;
+	regInEnable	[1]																	= regWriteEnable;
+	regInNum		[3*`RegNumWidth-1	:	2*`RegNumWidth] = regWriteNum;
 	case (forwardB)
-		2'b00: regOutData1[1] = regReadData1;
-		2'b01: regOutData1[1] = regInData[0];
-		2'b11,2'b10: regOutData1[1] = data;
-		default: regOutData1[1] = regReadData1;
+		2'b00:				regOutData1[2*`DataWidth-1:`DataWidth] = regReadData1;
+		2'b01:				regOutData1[2*`DataWidth-1:`DataWidth] = regInData[`DataWidth-1:0];
+		2'b11,2'b10:	regOutData1[2*`DataWidth-1:`DataWidth] = data;
+		default:			regOutData1[2*`DataWidth-1:`DataWidth] = regReadData1;
 	endcase
 	if (reset) begin
 		aluOp = `ADD;
 		aluX = 0;
 		aluY = 0;
-		state[1] = `IDLE;
+		state[2*`StateWidth-1:`StateWidth] = `IDLE;
 	end
 	else if (clk)
 	begin
@@ -75,17 +75,17 @@ begin
 					endcase
 					case (forwardA)
 						2'b00: aluX = regReadData0;
-						2'b01: aluX = regInData[0];
+						2'b01: aluX = regInData[`DataWidth-1:0];
 						2'b11,2'b10: aluX = data;
 						default: aluX = regReadData0;
 					endcase
 					case (forwardB)
 						2'b00: aluY = regReadData1;
-						2'b01: aluY = regInData[0];
+						2'b01: aluY = regInData[`DataWidth-1:0];
 						2'b11,2'b10: aluY = data;
 						default: aluY = regReadData1;
 					endcase
-					state[1] = `RegWrite;
+					state[2*`StateWidth-1:`StateWidth] = `RegWrite;
 				end
 				7'b0010011: // FMT I
 				begin
@@ -101,36 +101,36 @@ begin
 					endcase
 					case (forwardA)
 						2'b00: aluX = regReadData0;
-						2'b01: aluX = regInData[0];
+						2'b01: aluX = regInData[`DataWidth-1:0];
 						2'b11,2'b10: aluX = data;
 						default: aluX = regReadData0;
 					endcase
 					aluY = imm;
-					state[1] = `RegWrite;
+					state[2*`StateWidth-1:`StateWidth] = `RegWrite;
 				end
 				7'b0000011: // FMT I lb lh lw lbu lhu
 				begin
 					case (forwardA)
 						2'b00: aluX = regReadData0;
-						2'b01: aluX = regInData[0];
+						2'b01: aluX = regInData[`DataWidth-1:0];
 						2'b11,2'b10: aluX = data;
 						default: aluX = regReadData0;
 					endcase
 					aluY = imm;
 					aluOp = `ADD;
-					state[1] = `MemReadRegWrite; // Read memory and write register
+					state[2*`StateWidth-1:`StateWidth] = `MemReadRegWrite; // Read memory and write register
 				end
 				7'b0100011: // FMT S sb sh sw
 				begin
 					case (forwardA)
 						2'b00: aluX = regReadData0;
-						2'b01: aluX = regInData[0];
+						2'b01: aluX = regInData[`DataWidth-1:0];
 						2'b11,2'b10: aluX = data;
 						default: aluX = regReadData0;
 					endcase
 					aluY = imm;
 					aluOp = `ADD;
-					state[1]	= `MemWrite;
+					state[2*`StateWidth-1:`StateWidth]	= `MemWrite;
 				end
 				7'b1100011: // FMT B
 				begin
@@ -144,86 +144,80 @@ begin
 					endcase
 					case (forwardA)
 						2'b00: aluX = regReadData0;
-						2'b01: aluX = regInData[0];
+						2'b01: aluX = regInData[`DataWidth-1:0];
 						2'b11,2'b10: aluX = data;
 						default: aluX = regReadData0;
 					endcase
 					case (forwardB)
 						2'b00: aluY = regReadData1;
-						2'b01: aluY = regInData[0];
+						2'b01: aluY = regInData[`DataWidth-1:0];
 						2'b11,2'b10: aluY = data;
 						default: aluY = regReadData1;
 					endcase
-					state[1] = `PCSelectWrite;
+					state[2*`StateWidth-1:`StateWidth] = `PCSelectWrite;
 				end
 				7'b1101111: // FMT J jal
 				begin
-					aluX = pcData[0];
+					aluX = pcData[`AddrWidth-1:0];
 					aluY = imm;
 					aluOp = `ADD;
-					state[1]	= `PCWrite;
+					state[2*`StateWidth-1:`StateWidth]	= `PCWrite;
 				end
 				7'b1100111: // FMT I jalr
 				begin
 					case (forwardA)
 						2'b00: aluX = regReadData0;
-						2'b01: aluX = regInData[0];
+						2'b01: aluX = regInData[`DataWidth-1:0];
 						2'b11,2'b10: aluX = data;
 						default: aluX = regReadData0;
 					endcase
 					aluY = imm;
 					aluOp = `ADD;
-					state[1]	=	`PCWrite;
+					state[2*`StateWidth-1:`StateWidth]	=	`PCWrite;
 				end
 				7'b0110111: // FMT U lui
 				begin
-					state[1]	=	`LuiRegWrite;
+					state[2*`StateWidth-1:`StateWidth]	=	`LuiRegWrite;
 				end
 				7'b0010111: // FMT U auipc
 				begin
-					aluX = pcData[0];
+					aluX = pcData[`AddrWidth-1:0];
 					aluY = imm;
 					aluOp	=	`ADD;
-					state[1]	=	`RegWrite;
+					state[2*`StateWidth-1:`StateWidth]	=	`RegWrite;
 				end
-				default: state[1] = `IDLE;
+				default: state[2*`StateWidth-1:`StateWidth] = `IDLE;
 			endcase
 		end
 		else begin
 			aluX = 0;
 			aluY = 0;
 			aluOp	= `ADD;
-			state[1] = `IDLE;
+			state[2*`StateWidth-1:`StateWidth] = `IDLE;
 		end
 	end
 end
 
 always @(posedge clk)
 begin
-	aluOut[0] <= reset ? 0 : aluOut[1];
-	func3Data[0] <= reset ? 0 : func3Data[1];
-	immData[0] <= reset ? 0 : immData[1];
-	pcData[0] <= reset ? 0 : pcData[1];
-	pcData[1] <= reset ? 0 : pcData[2];
-	regInData[0] <= reset ? 0 : regInData[1];
-	regInEnable[0] <= reset ? 0 : regInEnable[1];
-	regInNum[0] <= reset ? 0 : regInNum[1];
-	regInNum[1] <= reset ? 0 : regInNum[2];
-	regOutData1[0] <= reset ? 0 : regOutData1[1];
-	state[0] <= reset ? 0 : hazard ? 0 : state[1];
+	aluOut			[`DataWidth-1			:0]						<= reset ? 0 : aluOut			[2*`DataWidth-1:`DataWidth];
+	func3Data		[`Func3Width-1		:0]						<= reset ? 0 : func3Data	[2*`Func3Width-1:`Func3Width];
+	immData			[`DataWidth-1			:0]						<= reset ? 0 : immData		[2*`DataWidth-1:`DataWidth];
+	pcData			[2*`AddrWidth-1		:`AddrWidth]	<= reset ? 0 : pcData			[3*`AddrWidth-1:2*`AddrWidth];
+	pcData			[`AddrWidth-1			:0]						<= reset ? 0 : pcData			[2*`AddrWidth-1:`AddrWidth];
+	regInData		[`DataWidth-1			:0]						<= reset ? 0 : regInData	[2*`DataWidth-1:`DataWidth];
+	regInEnable	[0]															<= reset ? 0 : regInEnable[1];
+	regInNum		[2*`RegNumWidth-1	:`RegNumWidth]<= reset ? 0 : regInNum		[3*`RegNumWidth-1:2*`RegNumWidth];
+	regInNum		[`RegNumWidth-1		:0]						<= reset ? 0 : regInNum		[2*`RegNumWidth-1:`RegNumWidth];
+	regOutData1	[`DataWidth-1			:0]						<= reset ? 0 : regOutData1[2*`DataWidth-1:`DataWidth];
+	state				[`StateWidth-1		:0]						<= reset ? 0 : hazard ? 0 : state[2*`StateWidth-1:`StateWidth];
 end
 
 ALU	alu(aluOp, aluX, aluY, aluO);
 
-Controller control(clk, reset, forwardB, PC, immData[0], regOutData1[0], aluOut[0], func3Data[0], state[0], pcWriteEnable, regWriteEnable, pcWriteData, regWriteData, data,
-	memWatchAddr,
-	memWatchData
-);
+Controller control(clk, reset, forwardB, PC, immData[`DataWidth-1:0], regOutData1[`DataWidth-1:0], aluOut[`DataWidth-1:0], func3Data[`Func3Width-1:0], state[`StateWidth-1:0], pcWriteEnable, regWriteEnable, pcWriteData, regWriteData, data, memWatchAddr, memWatchData);
 
 Forward Forwarding(clk, reset, flush, regNum0, regNum1, regWriteNum, forwardA, forwardB);
 
-RegsFile RF(PC, clk, reset, regInEnable[0], regInData[0], regNum0, regNum1, regInNum[0], regReadData0, regReadData1,
-	regWatchNum,
-	regWatchData
-);
+RegsFile RF(PC, clk, reset, regInEnable[0], regInData[`DataWidth-1:0], regNum0, regNum1, regInNum[`RegNumWidth-1:0], regReadData0, regReadData1, regWatchNum, regWatchData);
 endmodule

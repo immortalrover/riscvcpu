a258120f6744c02aa6be38ec8288bc071fe54aac
immortalrover <immortalrover@protonmail.com>
2024-05-15 21:36:25 +0800

    fix

--- pipelinecpu-FPGA/Flush.v
+++ pipelinecpu-FPGA/Flush.v
@@ -1,15 +1,15 @@
 module Flush (
-	input			clk, reset, pcWriteEnable,
-	output		flush
+	input				clk, reset, pcWriteEnable,
+	output	reg	flush
 );
 
 reg [2:0] number;
-assign flush = number > 0;
+always @(*) flush = number[2];
 
-always @(posedge pcWriteEnable) number = 3;
+always @(posedge pcWriteEnable) number = 3'b101;
 
 always @(posedge clk)
 begin
-	number <= reset ? 0 : flush ? number - 1 : 0;
+	number <= reset ? 0 : flush ? number + 1 : 0;
 end
 endmodule

29bbe16f983e252ba555d2f5266fa0e14cdb4e49
immortalrover <immortalrover@protonmail.com>
2024-05-21 00:18:02 +0800

    fix

--- Imcrying/flow.v
+++ Imcrying/flow.v
@@ -4,24 +4,23 @@ module flow #(parameter NUM = 0, parameter WIDTH = 8) (
 	output	[(NUM+1)*WIDTH-1:0]	y
 );
 
-reg [(NUM+1)*WIDTH-1:0]	data1;
-wire [NUM*WIDTH-1:0]		data2 = data1[NUM*WIDTH-1:0];
+reg [(NUM+1)*WIDTH-1:0]	data;
+/* wire [NUM*WIDTH-1:0]		data2 = data[NUM*WIDTH-1:0]; */
 
 /* always @(*) begin */
-/* 	data2 <= data1[NUM*WIDTH-1:0]; */
+/* 	data2 <= data[NUM*WIDTH-1:0]; */
 /* end */
 
 always @(posedge clk, negedge rstn) begin
 	if (!rstn)	begin
-		data1 <= 0;
-		data2 <= 0;
+		data <= 0;
 	end
 	else if (clk) begin
-		if (en) data1[WIDTH-1:0] <= x;
-		else data1[(WIDTH-1:0] <= init_x;
-		if (NUM != 0) data1[(NUM+1)*WIDTH-1:WIDTH] <= data2;
+		if (en) data[WIDTH-1:0] <= x;
+		else data[(WIDTH-1:0] <= init_x;
+		if (NUM != 0) data[(NUM+1)*WIDTH-1:WIDTH] <= data[NUM*WIDTH-1:0];
 	end
 end
 
-assign y = data1;
+assign y = data;
 endmodule
